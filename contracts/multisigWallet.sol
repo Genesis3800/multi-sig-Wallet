b3409729bf967208fbac25b48bf2afd40af8b969
Priyank Gupta <Genesis3800>
2022-05-25 03:38:20 +0530

    contract almost finished

--- contracts/multisigWallet.sol
+++ contracts/multisigWallet.sol
@@ -53,11 +53,7 @@ contract MultiSigWallet {
 
     constructor(address[] memory _owners, uint256 _numConfirmationsRequired) {
         require(_owners.length > 0, "owners required");
-        require(
-            _numConfirmationsRequired > 0 &&
-                _numConfirmationsRequired <= _owners.length,
-            "invalid number of required confirmations"
-        );
+        require(_numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length,"invalid number of required confirmations");
 
         for (uint256 i = 0; i < _owners.length; i++) {
             address owner = _owners[i];
